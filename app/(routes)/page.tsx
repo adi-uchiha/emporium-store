f750b8e2306bd44563933342fc1d36b1dcbc5df9
adi-uchiha <adi.cs.night@gmail.com>
2023-08-17 20:20:23 +0530

    change billboard id

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("57f79e38-a1f8-4c4f-9336-ae990dcd5d40");
 
   return (
     <Container>
